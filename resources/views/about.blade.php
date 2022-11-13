17fffa437cc607602659896e0651f6813c28b326
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2022-11-13 14:55:50 -0300

    pantillas

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    about
-</body>
-</html>
+@extends('layouts.app')
+@section('title','About')
+@section('meta-description','About meta description')
+
+@section('content')
+<h1>About</h1>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-</head>
-<body>
-    <ul>
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('blog') }}">Blog</a></li>
-        <li><a href="{{ route('about') }}">About</a></li>
-        <li><a href="{{ route('contact') }}">Contact</a></li>
-    </ul>
-</body>
-</html>
+@extends('layouts.app')
+@section('title','Home')
+@section('meta-description','Home meta description')
+
+@section('content')
+<h1>Inicio</h1>
+
+@endsection
